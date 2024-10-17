48e1422c2e4f45d6121f92e401f3005a07c0e0ed
rahulray8518 <rahulray8518@gmail.com>
2024-10-17 10:11:41 +0000

    idx-1

--- Star-Rating/star-rating-next/app/component/starRating.tsx
+++ Star-Rating/star-rating-next/app/component/starRating.tsx
@@ -5,15 +5,23 @@ const broderr = 'border border-solid border-black'
 
 const StarRating = () => {
 
-    const [hover,setHover] = useState(null)
+    const [hover,setHover] = useState(0);
+    const [rating,setRating] = useState(0);
+
    const arr = [1,2,3,4,5]
   return (
 
-         <div className={`flex h-[auto] w-auto text-[30px] justify-center ${broderr}`}>
+         <div className={`flex h-[auto] w-auto text-[50px] justify-center ${broderr}`}>
       {
         arr.map((value,index) => (
-          <button key={index} className={`flex m-2 ${broderr}`}>
-            <span className={`${hover ? 'text-yellow-500' : ''}`}>
+          <button key={index} 
+          className={`flex m-2 ${broderr}`}
+          onMouseOver={() => setHover(value)}
+          onMouseLeave={() => setHover(0)}
+          onClick={()=>setRating(value)}
+          >
+            {hover}
+            <span className={`${rating <= hover ? 'text-yellow-500' : ''}`}>
               &#9733;
             </span>
           </button>
