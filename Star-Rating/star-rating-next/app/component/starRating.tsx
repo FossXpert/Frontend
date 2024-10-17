14ac5ff63c39b37a450fa5b0ee3249bfd05440a0
FossXpert <FossXpert@outlook.com>
2024-10-17 13:48:30 +0530

    Refactor star rating buttons and add star rating component

---
+++ Star-Rating/star-rating-next/app/component/starRating.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const StarRating = (props: Props) => {
+  return (
+    <div>starRating</div>
+  )
+}
+
+export default StarRating
--- Star-Rating/star-rating-next/app/page.tsx
+++ Star-Rating/star-rating-next/app/page.tsx
@@ -1,18 +1,9 @@
+import StarRating from "./component/starRating";
 
 export default function Home() {
-  const arr = [1,2,3,4,5];
-
   return (
-    <div className={`flex justify-center`}>
-      {
-        arr.map((value,index) => (
-          <button key={index}>
-            <span>
-              &#9733;
-            </span>
-          </button>
-        ))
-      }
-    </div>
+   <>
+   <StarRating/>
+   </>
   );
 }
