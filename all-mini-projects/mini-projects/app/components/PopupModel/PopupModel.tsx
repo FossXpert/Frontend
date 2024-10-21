7a6e8e932592cd22d74c7a6205636f36ad3ea744
rahulray8518 <rahulray8518@gmail.com>
2024-10-21 08:32:46 +0000

    commit-1

--- all-mini-projects/mini-projects/app/components/PopupModel/PopupModel.tsx
+++ all-mini-projects/mini-projects/app/components/PopupModel/PopupModel.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 const PopupModel = () => {
   return (
-    <div>PopupModel</div>
+    <div>
+      
+    </div>
   )
 }
 
--- all-mini-projects/mini-projects/app/page.tsx
+++ all-mini-projects/mini-projects/app/page.tsx
@@ -3,6 +3,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
+      <div className="flex flex-col h-[100vh]">
+        <Link href={'/star-rating'}>Star Rating</Link>
+        <Link href={'/popup-model'}>Popup Model</Link>
+      </div>
       <div>
       </div>
     </>
