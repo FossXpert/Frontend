fbbe00fa2d0f47507c7a5abafaa8505b1d1d0c23
rahulray8518 <rahulray8518@gmail.com>
2024-11-16 14:46:46 +0000

    d

--- all-mini-projects/mini-projects/app/components/PopoverModel/PopoverModel.tsx
+++ all-mini-projects/mini-projects/app/components/PopoverModel/PopoverModel.tsx
@@ -3,8 +3,9 @@
 const PopoverModel = () => {
     return (
         <>
-        <div>
-            Hello
+        <div className="flex flex-col justify-center items-center">
+            <h1 className="text-5xl font-bold">Popover Model</h1>
+            <button className="w-[5rem] h-[2rem]">Hover Me</button>
         </div>
         </>
     )
