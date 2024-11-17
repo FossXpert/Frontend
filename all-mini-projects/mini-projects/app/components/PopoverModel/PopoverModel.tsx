42a893b98cc9e61e3453d21e20fa7bdff5377360
rahulray8518 <rahulray8518@gmail.com>
2024-11-17 04:58:58 +0000

    Model Created

--- all-mini-projects/mini-projects/app/components/PopoverModel/PopoverModel.tsx
+++ all-mini-projects/mini-projects/app/components/PopoverModel/PopoverModel.tsx
@@ -1,11 +1,21 @@
-
+import { useState } from "react";
+import Model from "./Model";
 
 const PopoverModel = () => {
+
+    const [showModel,setShowModel] = useState(false);
+
+
     return (
         <>
         <div className="flex flex-col justify-center items-center">
             <h1 className="text-5xl font-bold">Popover Model</h1>
-            <button className="m-2 w-[6rem] h-[2.5rem] bg-green-500 text-white rounded-[5px] hover:bg-violet-600">Hover Me</button>
+            <button className={`m-2 w-[6rem] h-[2.5rem] bg-green-500 text-white rounded-[5px] hover:bg-violet-600`}
+            onMouseEnter={()=>setShowModel(true)}
+            onMouseLeave={()=>setShowModel(false)}>Hover Me</button>
+            {
+                showModel && <div> <Model/> </div>
+            }
         </div>
         </>
     )
--- all-mini-projects/mini-projects/app/popover-model/page.tsx
+++ all-mini-projects/mini-projects/app/popover-model/page.tsx
@@ -1,5 +1,5 @@
+'use client'
 import PopoverModel from "../components/PopoverModel/PopoverModel"
-
 
 const page = () => {
     return (
