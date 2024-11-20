242cd11ba7135110773459ee84b047b0a1d5977d
rahulray8518 <rahulray8518@gmail.com>
2024-11-20 07:52:54 +0000

    n

--- all-mini-projects/mini-projects/app/components/PopoverModel/PopoverModel.tsx
+++ all-mini-projects/mini-projects/app/components/PopoverModel/PopoverModel.tsx
@@ -1,26 +1,23 @@
 import { useState } from "react";
 import Model from "./Model";
-import Popup from "./Popup";
-
 const PopoverModel = () => {
 
-    const [showModel,setShowModel] = useState(false);
-    const [popup,setPopup] = useState(true);
-    
-    
+    const [showModel, setShowModel] = useState(false);
+    // const [popup, setPopup] = useState(false);
+
     return (
         <>
-        <div className="flex flex-col pt-5 items-center gap-4 h-[100vh] bg-[#14161b]">
-            <h1 className="text-5xl font-bold text-white">Popover Model</h1>
-            <button className={`m-2 w-[6rem] h-[2.5rem] bg-green-500 text-white rounded-[5px] hover:bg-violet-600`}
-            onClick={()=>setShowModel(true)}>Hover Me</button>
-            {
-                showModel && <div> <Model showModal={showModel} setShowModal={setShowModel}/> </div>
-            }
-            {
-                popup && <div> <Popup popup={popup} setPopup={setPopup}/> </div>
-            }
-        </div>
+            <div className="flex flex-col pt-5 items-center gap-4 h-[100vh] bg-[#14161b]">
+                <h1 className="text-5xl font-bold text-white">Popover Model</h1>
+                <button className={`m-2 w-[6rem] h-[2.5rem] bg-green-500 text-white rounded-[5px] hover:bg-violet-600`}
+                    onClick={() => setShowModel(true)}>Hover Me</button>
+                {
+                    showModel && <div> <Model showModal={showModel} setShowModal={setShowModel} /> </div>
+                }
+                {/* {
+                    popup && <div> <Popup popup={popup} setPopup={setPopup} /> </div>
+                } */}
+            </div>
         </>
     )
 }
