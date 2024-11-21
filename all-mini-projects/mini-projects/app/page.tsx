369b8d45f9f814cfa81b777ca6ab19128e470e7e
rahulray8518 <rahulray8518@gmail.com>
2024-11-21 04:28:02 +0000

    Commit

--- all-mini-projects/mini-projects/app/page.tsx
+++ all-mini-projects/mini-projects/app/page.tsx
@@ -1,20 +1,19 @@
 import Link from "next/link";
 
 export default function Home() {
-  const arraysOfLinks = [{
-    url : [
-      'star-rating','popup-model','popover-model','accordion'
-    ],
-    title : [
-      'Star Rating','Popup Model','Popover Model','Accordion'
-    ]}
+
+  const arraysOfLinks = [
+    {url : 'star-rating',text:'Star Rating'},
+    {url : 'popup-model',text:'Popup Model'},
+    {url : 'popover-model',text:'Popover Model'},
+    {url : 'accordion',text:'Accordion'}
   ]
   return (
     <>
-      <div className="flex justify-center items-center font-bold text-xl h-[100vh]">
+      <div className="flex flex-col justify-center items-center font-bold text-xl h-[100vh]">
         {
           arraysOfLinks.map((value, index) => (
-            
+            <Link className={`hover:text-violet-500 hover:font-extrabold`} href={value.url} key={index}>{value.text}</Link>
           ))
         }
       </div>
