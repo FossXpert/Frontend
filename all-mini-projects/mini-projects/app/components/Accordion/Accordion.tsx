3290c4d13d64af1439d1026269edd7d209b93c43
rahulray8518 <rahulray8518@gmail.com>
2024-11-21 13:35:44 +0000

    f

--- all-mini-projects/mini-projects/app/components/Accordion/Accordion.tsx
+++ all-mini-projects/mini-projects/app/components/Accordion/Accordion.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import { FaPlus } from 'react-icons/fa';
 
-
-  
- 
 const Accordion = () => {
+
+  const faq = [
+    {
+      question : 'What is virtual DOM in React ?',
+      answer : "The Virtual DOM acts as an intermediary. It's a JavaScript object that mirrors the structure of the real DOM"
+    },
+    {
+      question : 'What is virtual DOM in React ?',
+      answer : "The Virtual DOM acts as an intermediary. It's a JavaScript object that mirrors the structure of the real DOM"
+    },
+    {
+      question : 'What is virtual DOM in React ?',
+      answer : "The Virtual DOM acts as an intermediary. It's a JavaScript object that mirrors the structure of the real DOM"
+    },
+    {
+      question : 'What is virtual DOM in React ?',
+      answer : "The Virtual DOM acts as an intermediary. It's a JavaScript object that mirrors the structure of the real DOM"
+    },
+    {
+      question : 'What is virtual DOM in React ?',
+      answer : "The Virtual DOM acts as an intermediary. It's a JavaScript object that mirrors the structure of the real DOM"
+    }
+  ]
+
+
   return (
-    <div>Accordion</div>
+    <div className='flex flex-col items-center gap-5'>
+        <h1 className='text-5xl font-bold '>React Interview Questions</h1>
+        <div className={`w-[50vh] h-auto border border-solid border-black`}>
+            {
+              faq.map((value,index) => (
+                  
+              ))
+            }
+        </div>
+    </div>
   )
 }
 
